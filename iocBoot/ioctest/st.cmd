787796211359d999a630c7d34c5b635fd89a8c1f
Michael Davidsaver <mdavidsaver@gmail.com>
2016-06-06 19:51:34 -0400

    build example IOC

--- iocBoot/ioctest/st.cmd
+++ iocBoot/ioctest/st.cmd
@@ -1,18 +1,43 @@
-#!../../bin/linux-x86_64-debug/warp
+#!../../bin/linux-x86_64-debug/test
 
-#- You may have to change warp to something else
-#- everywhere it appears in this file
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "3000100")
 
-#< envPaths
+# Prefix for all records
+epicsEnvSet("PREFIX", "13SIM1:")
+# The port name for the detector
+epicsEnvSet("PORT",   "SIM1")
+# The queue size for all plugins
+epicsEnvSet("QSIZE",  "20")
+# The maximim image width; used for row profiles in the NDPluginStats plugin
+epicsEnvSet("XSIZE",  "2000")
+# The maximim image height; used for column profiles in the NDPluginStats plugin
+epicsEnvSet("YSIZE",  "1500")
+# The maximum number of time series points in the NDPluginStats plugin
+epicsEnvSet("NCHANS", "2048")
 
-## Register all support components
-dbLoadDatabase("../../dbd/warp.dbd",0,0)
-warp_registerRecordDeviceDriver(pdbbase) 
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "../../db")
 
-## Load record instances
-dbLoadRecords("../../db/warp.db","user=mdavidsaver")
+dbLoadDatabase("../../dbd/test.dbd")
+test_registerRecordDeviceDriver(pdbbase) 
+
+simDetectorConfig("$(PORT)", $(XSIZE), $(YSIZE), 1, 0, 0)
+dbLoadRecords("simDetector.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+
+NDWarpConfigure("Warp1", 3, 0, "$(PORT)", 0)
+dbLoadRecords("NDWarp.template","P=$(PREFIX),R=warp1:,PORT=Warp1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT)")
+
+NDStatsConfigure("STATS1", $(QSIZE), 0, "Warp1", 0, 0, 0)
+dbLoadRecords("NDStats.template",     "P=$(PREFIX),R=Stats1:,  PORT=STATS1,ADDR=0,TIMEOUT=1,HIST_SIZE=256,XSIZE=$(XSIZE),YSIZE=$(YSIZE),NCHANS=$(NCHANS),NDARRAY_PORT=Warp1")
+
+NDStdArraysConfigure("Image1", 3, 0, "Warp1", 0)
+dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=Warp1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=3000000")
 
 iocInit()
 
-## Start any sequence programs
-#seq sncwarp,"user=mdavidsaver"
+dbpf "$(PREFIX)cam1:ImageMode" "Single"
+dbpf "$(PREFIX)cam1:ArrayCallbacks" "Enable"
+dbpf "$(PREFIX)warp1:EnableCallbacks" "Enable"
+dbpf "$(PREFIX)image1:EnableCallbacks" "Enable"
+
+dbpf "$(PREFIX)warp1:CenterX" "1000"
+dbpf "$(PREFIX)warp1:CenterY" "850"
